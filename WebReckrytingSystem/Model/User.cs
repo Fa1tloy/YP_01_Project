900c84ae55eeb8cf49469eeedf81cdb84d43ceb0
Fa1tloy <baranovdaniil534@gmail.com>
2025-10-15 23:02:00 +0300

    Добавил модель данных описывающая users, а также добавил строку подключения к базе данных в файле  appseting.json

---
+++ WebReckrytingSystem/Model/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebReckrytingSystem.Model
+{
+    public class User
+    {
+        public required string Email { get; set; }
+
+
+        public required string PasswordHash { get; set; }
+
+        [Required(ErrorMessage = "Имя обязательно для заполнения.")]
+        [StringLength(50, ErrorMessage = "Имя не должно превышать 50 символов.")]
+        public required string FirstName { get; set; }
+
+
+        [Required(ErrorMessage = "Фамилия обязательна для заполнения.")]
+        [StringLength(50, ErrorMessage = "Фамилия не должна превышать 50 символов.")]
+        public required string LastName { get; set; }
+
+        public enum Role {job_seeker , employer }
+
+        
+
+
+    }
+}
