9a7d61e3bf9c35621cb2663fddfff5c2227776a5
Fa1tloy <baranovdaniil534@gmail.com>
2025-10-30 19:18:44 +0300

    Добавил пару пользователей с разными ролями и сделал настройку тестового окружения

--- WebReckrytingSystem.Testing/Pages/Account/LoginModelTests.cs
+++ WebReckrytingSystem.Testing/Pages/Account/LoginModelTests.cs
@@ -1,10 +1,52 @@
-namespace WebReckrytingSystem.Testing.Pages.Account;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Security.Claims;
+using WebReckrytingSystem.Data;
+using WebReckrytingSystem.Models;
+using WebReckrytingSystem.Pages.Account;
+using WebReckrytingSystem.Services;
 
-[TestClass]
-public class LoginModelTests
+namespace WebReckrytingSystem.Tests.Pages.Account
 {
-    [TestMethod]
-    public void TestMethod1()
+    [TestClass]
+    public class LoginModelTests
     {
+        private Mock<UserService> _mockUserService;
+        private LoginModel _loginModel;
+
+        private readonly User _seeker = new User
+        {
+            Email = "petrov.ivan@example.com",
+            FirstName = "Иван",
+            LastName = "Петров",
+            Role = "job_seeker"
+        };
+
+        private readonly User _employer = new User
+        {
+            Email = "hr@techcompany.ru",
+            FirstName = "Ольга",
+            LastName = "Смирнова",
+            Role = "employer"
+        };
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockUserService = new Mock<UserService>(Mock.Of<IUserRepository>());
+            _loginModel = new LoginModel(_mockUserService.Object)
+            {
+                PageContext = new PageContext
+                {
+                    HttpContext = new DefaultHttpContext()
+                }
+            };
+        }
+
+        
     }
 }
