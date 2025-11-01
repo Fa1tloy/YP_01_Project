414909bdbb1bf082257afbcabb62c79c67d891a2
Fa1tloy <baranovdaniil534@gmail.com>
2025-11-01 11:47:50 +0300

    Сделал тесты на возврат личного кабинета

--- WebReckrytingSystem.Tests/Pages/Account/AccessControlTests.cs
+++ WebReckrytingSystem.Tests/Pages/Account/AccessControlTests.cs
@@ -1,12 +1,114 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Security.Claims;
+using WebReckrytingSystem.Pages.Account;
 
 namespace WebReckrytingSystem.Tests.Pages.Account
 {
-    internal class Class2
+    [TestClass]
+    public class AccessControlTests
     {
+        [TestMethod]
+        public void JobSeekerDashboard_UnauthenticatedUser_RedirectsToLogin()
+        {
+            // Arrange
+            var pageModel = new JobSeekerDashboardModel(Mock.Of<WebReckrytingSystem.Services.IResumeService>())
+            {
+                PageContext = new PageContext
+                {
+                    HttpContext = new DefaultHttpContext()
+                }
+            };
+
+            // Act
+            var result = pageModel.OnGet();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToPageResult));
+            var redirectResult = (RedirectToPageResult)result;
+            Assert.AreEqual("/Account/Login", redirectResult.PageName);
+        }
+
+        [TestMethod]
+        public void EmployerDashboard_UnauthenticatedUser_RedirectsToLogin()
+        {
+            // Arrange
+            var pageModel = new EmployerDashboardModel()
+            {
+                PageContext = new PageContext
+                {
+                    HttpContext = new DefaultHttpContext()
+                }
+            };
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual("Пользователь", pageModel.UserFirstName);
+        }
+
+        [TestMethod]
+        public void JobSeekerDashboard_AuthenticatedSeeker_ReturnsPage()
+        {
+            // Arrange
+            var httpContext = new DefaultHttpContext();
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Email, "seeker@test.com"),
+                new Claim(ClaimTypes.GivenName, "Иван"),
+                new Claim(ClaimTypes.Role, "job_seeker")
+            }, "TestAuth"));
+
+            httpContext.User = user;
+
+            var pageModel = new JobSeekerDashboardModel(
+                Mock.Of<WebReckrytingSystem.Services.IResumeService>())
+            {
+                PageContext = new PageContext
+                {
+                    HttpContext = httpContext
+                }
+            };
+
+            // Act
+            var result = pageModel.OnGet();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(PageResult));
+            Assert.AreEqual("Иван", pageModel.UserFirstName);
+        }
+
+        [TestMethod]
+        public void EmployerDashboard_AuthenticatedEmployer_ReturnsPage()
+        {
+            // Arrange
+            var httpContext = new DefaultHttpContext();
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Email, "employer@test.com"),
+                new Claim(ClaimTypes.GivenName, "Ольга"),
+                new Claim(ClaimTypes.Role, "employer")
+            }, "TestAuth"));
+
+            httpContext.User = user;
+
+            var pageModel = new EmployerDashboardModel()
+            {
+                PageContext = new PageContext
+                {
+                    HttpContext = httpContext
+                }
+            };
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual("Ольга", pageModel.UserFirstName);
+        }
     }
 }
