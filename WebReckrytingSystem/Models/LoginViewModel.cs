5bcee68d5a6b5a2d64935f121e3a0d2b5c1ef390
Fa1tloy <baranovdaniil534@gmail.com>
2025-11-06 17:21:20 +0300

    Добавил модель авторизации пользователя

--- WebReckrytingSystem/Models/LoginViewModel.cs
+++ WebReckrytingSystem/Models/LoginViewModel.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebReckrytingSystem.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email обязателен")]
+        [EmailAddress(ErrorMessage = "Неверный формат email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Пароль обязателен")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        public bool RememberMe { get; set; }
+    }
+}
