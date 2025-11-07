54bb788ee031f430f2a8bb3c47d50667c53ac107
Fa1tloy <baranovdaniil534@gmail.com>
2025-11-07 10:07:55 +0300

    Добавил страницы для входа и выхода

---
+++ WebReckrytingSystem/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebReckrytingSystem.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
