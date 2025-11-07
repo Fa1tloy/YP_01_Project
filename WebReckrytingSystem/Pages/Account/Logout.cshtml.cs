6ce1593c280a382f4a2ad5dc1ce2bf65e191b247
Fa1tloy <baranovdaniil534@gmail.com>
2025-11-07 10:12:39 +0300

    Сделал выход из системы

--- WebReckrytingSystem/Pages/Account/Logout.cshtml.cs
+++ WebReckrytingSystem/Pages/Account/Logout.cshtml.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace WebReckrytingSystem.Pages.Account
 {
     public class LogoutModel : PageModel
     {
-        public void OnGet()
+        public async Task<IActionResult> OnPostAsync()
         {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToPage("/Index");
         }
     }
 }
