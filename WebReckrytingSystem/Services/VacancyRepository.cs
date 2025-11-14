b947abf683822e4c87a2eb075f89f45592273516
Z <pgbc303@gmail.com>
2025-11-14 10:49:47 +0300

    Реализовал реп вакансий

--- WebReckrytingSystem/Services/VacancyRepository.cs
+++ WebReckrytingSystem/Services/VacancyRepository.cs
@@ -1,6 +1,66 @@
-﻿namespace WebReckrytingSystem.Services
+﻿using Microsoft.EntityFrameworkCore;
+using WebReckrytingSystem.Data;
+using WebReckrytingSystem.Models;
+
+namespace WebReckrytingSystem.Services
 {
-    public class VacancyRepository
+    public class VacancyRepository : IVacancyRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public VacancyRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Vacancy? GetByCompanyAndTitle(string companyName, string title)
+        {
+            return _context.Vacancies
+                .Include(v => v.Company)
+                .Include(v => v.Author)
+                .FirstOrDefault(v => v.CompanyName == companyName && v.Title == title);
+        }
+
+        public ICollection<Vacancy> GetByAuthor(string authorEmail)
+        {
+            return _context.Vacancies
+                .Include(v => v.Company)
+                .Where(v => v.AuthorEmail == authorEmail)
+                .ToList();
+        }
+
+        public ICollection<Vacancy> GetPublishedVacancies()
+        {
+            return _context.Vacancies
+                .Include(v => v.Company)
+                .Include(v => v.Author)
+                .ToList();
+        }
+
+        public Vacancy Save(Vacancy vacancy)
+        {
+            _context.Vacancies.Add(vacancy);
+            _context.SaveChanges();
+            return vacancy;
+        }
+
+        public Vacancy Update(Vacancy vacancy)
+        {
+            _context.Vacancies.Update(vacancy);
+            _context.SaveChanges();
+            return vacancy;
+        }
+
+        public bool Delete(string companyName, string title)
+        {
+            var vacancy = GetByCompanyAndTitle(companyName, title);
+            if (vacancy != null)
+            {
+                _context.Vacancies.Remove(vacancy);
+                _context.SaveChanges();
+                return true;
+            }
+            return false;
+        }
     }
 }
