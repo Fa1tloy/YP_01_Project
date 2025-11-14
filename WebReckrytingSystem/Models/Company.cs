ec8c0435b2bbf408c3f3c545df9f991ab74c6b48
Z <pgbc303@gmail.com>
2025-11-14 10:31:13 +0300

    Добавил модель компании в соответствии с таблицей в базе данных

---
+++ WebReckrytingSystem/Models/Company.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebReckrytingSystem.Models
+{
+    public class Company
+    {
+        [Key]
+        [StringLength(255)]
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public string? Website { get; set; }
+
+        public string? LogoUrl { get; set; }
+
+        public bool Verified { get; set; } = false;
+
+        // Навигационные свойства
+        public ICollection<Vacancy> Vacancies { get; set; } = new List<Vacancy>();
+    }
+}
