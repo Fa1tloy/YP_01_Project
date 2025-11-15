96e4d8731cf4ed82e01e87b7de84d653300c281f
Z <pgbc303@gmail.com>
2025-11-15 09:05:41 +0300

    Создаем функциональность для управления компаниями

---
+++ WebReckrytingSystem/Models/CreateCompanyViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebReckrytingSystem.Models
+{
+    public class CreateCompanyViewModel
+    {
+        [Required(ErrorMessage = "Название компании обязательно")]
+        [StringLength(255, ErrorMessage = "Название компании не должно превышать 255 символов")]
+        [Display(Name = "Название компании")]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(1000, ErrorMessage = "Описание не должно превышать 1000 символов")]
+        [Display(Name = "Описание компании")]
+        public string? Description { get; set; }
+
+        [Url(ErrorMessage = "Введите корректный URL")]
+        [Display(Name = "Веб-сайт")]
+        public string? Website { get; set; }
+    }
+}
