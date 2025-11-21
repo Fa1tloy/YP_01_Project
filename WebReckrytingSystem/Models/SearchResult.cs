142669ca519eeabca6bca00e591c07e085546817
Fa1tloy <baranovdaniil534@gmail.com>
2025-11-21 10:49:02 +0300

    Модель для поиска еще

---
+++ WebReckrytingSystem/Models/SearchResult.cs
@@ -0,0 +1,13 @@
+﻿namespace WebReckrytingSystem.Models
+{
+    public class SearchResult<T>
+    {
+        public ICollection<T> Items { get; set; } = new List<T>();
+        public int TotalCount { get; set; }
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+        public bool HasPreviousPage => Page > 1;
+        public bool HasNextPage => Page < TotalPages;
+    }
+}
