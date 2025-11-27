38b41faa1d5ece8fb31db122bd63242cf3de3ee4
Fa1tloy <baranovdaniil534@gmail.com>
2025-11-27 11:10:44 +0300

    Обновил интерфейс

--- WebReckrytingSystem/Services/IVacancyService.cs
+++ WebReckrytingSystem/Services/IVacancyService.cs
@@ -2,10 +2,11 @@
 
 namespace WebReckrytingSystem.Services
 {
+    // IVacancyService.cs
     public interface IVacancyService
     {
         ServiceResult<Vacancy> CreateVacancy(string authorEmail, CreateVacancyViewModel model);
-        ServiceResult<Vacancy> UpdateVacancy(string companyName, string title, CreateVacancyViewModel model);
+        ServiceResult<Vacancy> UpdateVacancy(string companyName, string title, string userEmail, CreateVacancyViewModel model);
         Vacancy? GetVacancy(string companyName, string title);
         ICollection<Vacancy> GetUserVacancies(string authorEmail);
         ICollection<Vacancy> GetAllVacancies();
