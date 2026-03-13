3e3d4beb1b96d0695bb17d0b1af9a7f518127204
Fa1tloy <baranovdaniil534@gmail.com>
2026-03-13 06:13:27 +0300

    Add backward-compatible SearchResult.Page alias

--- WebReckrytingSystem/Models/SearchResult.cs
+++ WebReckrytingSystem/Models/SearchResult.cs
@@ -5,6 +5,13 @@
         public ICollection<T> Items { get; set; } = new List<T>();
         public int TotalCount { get; set; }
         public int PageNumber { get; set; } = 1;
+
+        // Backward-compatible alias for older call sites
+        public int Page
+        {
+            get => PageNumber;
+            set => PageNumber = value;
+        }
         public int PageSize { get; set; } = 10;
         public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
         public bool HasPreviousPage => PageNumber > 1;
